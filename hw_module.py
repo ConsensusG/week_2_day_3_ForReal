d7a052f615ccaa6953731def907cb01f74ad1268
Justin Wescott <YungCircus@gmail.com>
2023-07-31 17:25:35 -0400

    first

---
+++ hw_module.py
@@ -0,0 +1,8 @@
+# Square Footage
+def area_calc(length, width):
+    return length*width
+
+# Circumference
+from math import pi
+def circumf_calc(radius):
+    return 2*pi*radius
---
+++ module.py
@@ -0,0 +1,2 @@
+def printName(name):
+    print ("Hello Mr/Ms {name}...we've been waiting for you!")
